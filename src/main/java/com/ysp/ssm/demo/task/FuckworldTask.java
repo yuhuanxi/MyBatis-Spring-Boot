4b65b6030abed755b4c556164fd6a89435a7fda0
huanxiyu <adobe1874@126.com>
2016-08-30 10:23:16 +0800

    分离task

--- src/main/java/com/ysp/ssm/demo/task/FuckworldTask.java
+++ src/main/java/com/ysp/ssm/demo/task/FuckworldTask.java
@@ -4,12 +4,64 @@ package com.ysp.ssm.demo.task;
  * Created by yuhuanxi on 16/8/27.
  */
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 
-@Component
+import java.util.HashMap;
+import java.util.Map;
+
+//@Component
+@Configuration
 public class FuckworldTask {
 
     public void print() {
         System.out.println("Executing fuckworld job.......");
+    }
+
+    @Autowired
+    @Qualifier("fJobDetailFactoryBean")
+    JobDetailFactoryBean fJobDetailFactoryBean;
+
+    @Autowired
+    @Qualifier("fCronTriggerFactoryBean")
+    CronTriggerFactoryBean fCronTriggerFactoryBean;
+
+    /**
+     * 触发器 2
+     *
+     * @return
+     */
+    @Bean
+    public CronTriggerFactoryBean fCronTriggerFactoryBean() {
+        CronTriggerFactoryBean trigger = new CronTriggerFactoryBean();
+        trigger.setJobDetail(fJobDetailFactoryBean.getObject());
+        trigger.setStartDelay(3000);
+        // TODO 该表达式可从数据库或者配置文件中获取
+        trigger.setCronExpression("0/5 * * * * ?");
+        return trigger;
+    }
+
+    /**
+     * jobDetail 2
+     *
+     * @return
+     */
+    @Bean
+    public JobDetailFactoryBean fJobDetailFactoryBean() {
+        JobDetailFactoryBean jobDetail = new JobDetailFactoryBean();
+        jobDetail.setJobClass(FuckworldJob.class);
+
+        // bring real ftask
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("ftask", new FuckworldTask());
+        jobDetail.setJobDataAsMap(map);
+
+        jobDetail.setDurability(true);
+        jobDetail.setName("fuckworld");
+        return jobDetail;
     }
 }
--- src/main/java/com/ysp/ssm/demo/task/QuartzConfiguration.java
+++ src/main/java/com/ysp/ssm/demo/task/QuartzConfiguration.java
@@ -7,12 +7,7 @@ package com.ysp.ssm.demo.task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
-import org.springframework.scheduling.quartz.JobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Configuration
@@ -28,94 +23,10 @@ public class QuartzConfiguration {
     @Bean
     public SchedulerFactoryBean schedulerFactoryBean() {
         SchedulerFactoryBean scheduler = new SchedulerFactoryBean();
-        //scheduler.setOverwriteExistingJobs(true);
-//        scheduler.setTriggers(cronTriggerFactoryBean().getObject(), cronTriggerFactoryBean2().getObject());
-        scheduler.setTriggers(htask.cronTriggerFactoryBean.getObject(), cronTriggerFactoryBean2().getObject());
-//        scheduler.setJobDetails(jobDetailFactoryBean().getObject(), jobDetailFactoryBean2().getObject());
-        scheduler.setJobDetails(htask.jobDetailFactoryBean.getObject(), jobDetailFactoryBean2().getObject());
+        scheduler.setOverwriteExistingJobs(true);
+        scheduler.setTriggers(htask.cronTriggerFactoryBean.getObject(), ftask.fCronTriggerFactoryBean.getObject());
+        scheduler.setJobDetails(htask.jobDetailFactoryBean.getObject(), ftask.fJobDetailFactoryBean.getObject());
         return scheduler;
     }
 
-    // Job is scheduled after every 5 sec
-
-    /**
-     * 触发器 1
-     *
-     * @return
-     */
-//    @Bean
-//    public CronTriggerFactoryBean cronTriggerFactoryBean() {
-//        CronTriggerFactoryBean trigger = new CronTriggerFactoryBean();
-//        trigger.setJobDetail(jobDetailFactoryBean().getObject());
-//        trigger.setStartDelay(3000);
-//        // TODO 该表达式可从数据库或者配置文件中获取
-//        trigger.setCronExpression("0/5 * * * * ?");
-//        return trigger;
-//    }
-
-    /**
-     * 触发器 2
-     *
-     * @return
-     */
-    @Bean
-    public CronTriggerFactoryBean cronTriggerFactoryBean2() {
-        CronTriggerFactoryBean trigger = new CronTriggerFactoryBean();
-        trigger.setJobDetail(jobDetailFactoryBean2().getObject());
-        trigger.setStartDelay(3000);
-        // TODO 该表达式可从数据库或者配置文件中获取
-        trigger.setCronExpression("0/5 * * * * ?");
-        return trigger;
-    }
-
-    // configure job bean
-
-    /**
-     * jobDetail 1
-     *
-     * @return
-     */
-//    @Bean
-//    public JobDetailFactoryBean jobDetailFactoryBean() {
-//        JobDetailFactoryBean jobDetail = new JobDetailFactoryBean();
-//        jobDetail.setJobClass(HelloworldJob.class);
-//
-//        // bring real htask
-//        Map<String, Object> map = new HashMap<String, Object>();
-//        map.put("htask", htask);
-//        jobDetail.setJobDataAsMap(map);
-//        jobDetail.setDurability(true);
-//        jobDetail.setName("helloworld");
-//        return jobDetail;
-//    }
-
-    /**
-     * jobDetail 2
-     *
-     * @return
-     */
-    @Bean
-    public JobDetailFactoryBean jobDetailFactoryBean2() {
-        JobDetailFactoryBean jobDetail = new JobDetailFactoryBean();
-        jobDetail.setJobClass(FuckworldJob.class);
-
-        // bring real ftask
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("ftask", ftask);
-        jobDetail.setJobDataAsMap(map);
-
-        jobDetail.setDurability(true);
-        jobDetail.setName("fuckworld");
-        return jobDetail;
-    }
-
-//    @Bean
-//    HelloworldTask htask() {
-//        return new HelloworldTask();
-//    }
-//
-//    @Bean
-//    FuckworldTask ftask() {
-//        return new FuckworldTask();
-//    }
 }
